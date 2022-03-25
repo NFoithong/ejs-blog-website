a32b71ff5f9722bcfc1b84e08129d6ba04cd6658
NFoithong <n.foithong1983@gmail.com>
2022-03-24 23:57:24 -0400

    add express routing

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const ejs = require("ejs");
 const res = require("express/lib/response");
+var _ = require('lodash');
+
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -67,6 +69,20 @@ app.post('/compose', (req, res) => {
     res.redirect('/');
 });
 
+// GET single post on page
+app.get('/posts/:postName', function(req, res) {
+    const postName = _.lowerCase([req.params.postName]);
+
+    posts.forEach(function(post) {
+        const storedTitle = post.title;
+
+        if (storedTitle === postName) {
+            console.log('Match Found!');
+        } else {
+            console.log('Not Match!');
+        }
+    });
+});
 
 
 
